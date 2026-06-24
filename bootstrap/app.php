31c4d18f55f365e3087775a4e8345e3cc5150132
EvilDemo127 <maungzawshein20@gmail.com>
2026-06-24 19:04:25 +0700

    Add render config files

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,23 +4,15 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-if (env('APP_ENV') === 'production') {
-    $app = Application::configure(basePath: $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__))
-        ->create();
-        
-    $app->useStoragePath('/tmp');
-    return $app;
-}
-
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware) {
+    ->withMiddleware(function (Middleware $middleware): void {
         //
     })
-    ->withExceptions(function (Exceptions $exceptions) {
+    ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
